5f5c393a3125152759cdf75cff2e12e09cd75a47
gitConstantineKulak <constantinekulak@proton.me>
2023-09-25 14:23:59 +0300

    fix: Content View not commited

---
+++ Posts/Views/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Posts
+//
+//  Created by Constantine Kulak on 13.09.2023.
+//
+
+import SwiftUI
+import Resolver
+
+struct ContentView: View {
+    @InjectedObject private var appStateManager: AppStateManager
+    @InjectedObject private var viewModel: LoginViewModel
+    @InjectedObject private var databaseViewModel: DatabaseViewModel
+    @InjectedObject private var storageViewModel: StorageViewModel
+
+    var body: some View {
+        switch appStateManager.isLoggedIn {
+        case .loggedIn:
+            MainPageView()
+        case .didNotProvideDetails:
+            ProvideDetailsView()
+        case .notLoggedIn:
+            LoginPageView()
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
